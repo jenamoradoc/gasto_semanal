744823808e25f9b255ec5ce1da0c84bef3a2faf9
jenamorado <jenamorado@grupoassa.com>
2019-02-04 12:10:49 -0300

    componentes para el control de presupuesto

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import '../css/App.css';
 import Header from './Header';
 import Formulario from './Formulario';
+import Listado from './Listado';
+import ControlPresupuesto from './ControlPresupuesto';
+import {validarPresupuesto} from '../helper';
 
 class App extends Component {
   
@@ -10,6 +13,27 @@ class App extends Component {
     restante: '',
     gastos: {}
   }
+  
+  //al inicio de la app esto se realiza primero
+  componentDidMount(){
+    this.obtenerPresupuesto();
+  }
+
+  obtenerPresupuesto = () =>{
+    let presupuesto = prompt('Cual es el presupuesto');
+
+    let resultado = validarPresupuesto(presupuesto);
+    if (resultado) {
+      this.setState({
+        presupuesto:presupuesto,
+        restante: presupuesto
+      })
+    }else{
+      console.log('Presupuesto no valido');
+      this.obtenerPresupuesto();
+    }
+  }
+
   //se agrega un nuevo gasto al state y se toma una copia del state actual
   agregarGasto = gasto => {
     const gastos = {...this.state.gastos};
@@ -35,7 +59,10 @@ class App extends Component {
               />
             </div>
             <div className="one-half column">
-
+              <Listado
+                gastos={this.state.gastos}
+              />
+              <ControlPresupuesto/>
             </div>
           </div>
         </div>
